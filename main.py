61775e80fbc797180cd1b0b9c338fd28170d5143
AlexLee729 <alexblee1118@gmail.com>
2024-09-22 15:41:02 -0400

    added files

---
+++ main.py
@@ -0,0 +1,95 @@
+from graphviz import Digraph
+from value import Value
+from nn import MLP
+
+def trace(root):
+  # builds a set of all nodes and edges in a graph
+  nodes, edges = set(), set()
+  def build(v):
+    if v not in nodes:
+      nodes.add(v)
+      for child in v._prev:
+        edges.add((child, v))
+        build(child)
+  build(root)
+  return nodes, edges
+
+def draw_dot(root):
+  dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'}) # LR = left to right
+  
+  nodes, edges = trace(root)
+  for n in nodes:
+    uid = str(id(n))
+    # for any value in the graph, create a rectangular ('record') node for it
+    dot.node(name = uid, label = "{ %s | data %.4f | grad %.4f}" % (n.label, n.data, n.grad), shape='record')
+    if n._op:
+      # if this value is a result of some operation, create an op node for it
+      dot.node(name = uid + n._op, label = n._op)
+      # and connect this node to it
+      dot.edge(uid + n._op, uid)
+
+  for n1, n2 in edges:
+    # connect n1 to the op node of n2
+    dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+  return dot
+
+#region ########### Simple Neuron Backpropagation ###########
+# inputs x1,x2
+x1 = Value(2.0, label='x1')
+x2 = Value(0.0, label='x2')
+# weights w1,w2
+w1 = Value(-3.0, label='w1')
+w2 = Value(1.0, label='w2')
+# bias of the neuron
+b = Value(6.8813735870195432, label='b')
+# x1*w1 + x2*w2 + b
+x1w1 = x1*w1; x1w1.label = 'x1*w1'
+x2w2 = x2*w2; x2w2.label = 'x2*w2'
+x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
+n = x1w1x2w2 + b; n.label = 'n'
+
+# --- tanh
+e = (2*n).exp()
+o = (e - 1) / (e + 1)
+# ---
+
+o.label = 'o'
+o.backward()
+# print(draw_dot(o))
+#endregion
+
+#region ########### MLP Version ###########
+x = [2.0, 3.0, -1.0]
+n = MLP(3, [4, 4, 1])
+n(x)
+xs = [
+  [2.0, 3.0, -1.0],
+  [3.0, -1.0, 0.5],
+  [0.5, 1.0, 1.0],
+  [1.0, 1.0, -1.0],
+]
+ys = [1.0, -1.0, -1.0, 1.0]
+
+for k in range(20):
+  
+  # forward pass
+  ypred = [n(x) for x in xs]
+  loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+  
+  # backward pass
+  for p in n.parameters():
+    p.grad = 0.0
+  loss.backward()
+  
+  # update
+  for p in n.parameters():
+    p.data += -0.1 * p.grad
+  
+  print(k, loss.data)
+
+dot = draw_dot(loss)
+dot_str = dot.source
+with open('mlp.svg', 'w') as f:
+    f.write(dot_str)
+#endregion
